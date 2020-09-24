a4942500c43a0ba3e13d7ec3e766f750dd76101b
Manya1998 <34962083+Manya1998@users.noreply.github.com>
2020-09-24 09:39:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const port =3000
+const parser = require('body-parser');
+
+const user_signup= require('./auth/user_signup')
+const merc_signup= require('./auth/merc_signup')
+const login= require('./auth/login')
+const user= require('./routes/user')
+const cont= require('./routes/cont')
+const subs= require('./routes/subs')
+mongoose.connect("mongodb+srv://gabbar329:mongo@cluster0-718ce.mongodb.net/user?retryWrites=true&w=majority");
+
+
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended:true}));
+
+app.use('*',function(req,res,next){
+    res.set('Access-Control-Allow-Origin','*');
+    res.set('Access-Control-Allow-Headers','content-type');
+
+        next();    
+        
+        });
+
+app.get('/',(req,res)=>{
+    res.send("Home")
+})
+
+app.use('/usersignup',user_signup)
+app.use('/mercsignup',merc_signup)
+app.use('/login',login)
+app.use('/user',user)
+app.use('/cont',cont)
+app.use('/subs',subs)
+
+
+
+
+
+app.listen(port,()=>
+{
+    console.log("Server Runnning on port: "+port)
+})
